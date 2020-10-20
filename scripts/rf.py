b6fd6ce059e9ffd58fbca024893372f0769392bd
Ayllonbe <ayllonbenitez.aaron@gmail.com>
2020-10-20 15:09:58 +0200

    preparing gridSearch for RF

--- scripts/rf.py
+++ scripts/rf.py
@@ -1,76 +1,119 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 13 2020
+Created on Tue Sep 22 11:24:52 2020
 
-@author: Aaron Ayllon Benitez
-@description: Task 3 to relation extraction project: calsification
+@author: aaron
 """
 
+import argparse
+import logging
+import os
+import sys
+import pickle
+from joblib import parallel_backend
+from joblib import register_parallel_backend
+from joblib import cpu_count
+from ipyparallel import Client
+from ipyparallel.joblib import IPythonParallelBackend
+
 import pandas as pd
-import numpy as np
-from collections import Counter
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_roc_curve
-import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
 
-def main(file):
-    np.random.seed(1234)
-    inputT = pd.read_csv(file,sep=",",keep_default_na=False)
-    inputT["Type"] = inputT["Type"].map({"Directed":1,"Undirected":0})
-    inputT["Link"] = inputT["Link"].map({"catalysis":0,"binding":1,"reaction":0,"regulation":0})
-    inputT = inputT.drop_duplicates()
-    inputT['iteraction'] = inputT['Source'].str.cat(inputT['Target'],sep="-")
-    arr = []
-    zero = inputT.loc[inputT['Link'] == 0]
-    ones = inputT.loc[inputT['Link'] == 1]
-    print(zero)
-    print(ones)
-    inter0 = zero['iteraction']
-    index0 = inter0.index
-    inter1 = ones['iteraction'].to_numpy()
-    for idx, interaction in np.ndenumerate(inter0):
-        if(interaction in inter1):
-            arr.append(index0[idx[0]])
-           
-    inputT = inputT.drop(arr)
-    
-    X = inputT.loc[:,"ARACNE_score":"irp_score"]
-    y         = inputT["Link"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    model = RandomForestClassifier(n_estimators=1000)
-    knn = KNeighborsClassifier(n_neighbors=9)
-    svc = SVC()
-    nn = MLPClassifier(max_iter=500)
-    
 
-    print("Training model 1.")
-    #train model
-    model.fit(X_train, y_train)
-    print("Training model 2.")
-    knn.fit(X_train, y_train)
-    print("training model 3.")
-    svc.fit(X_train, y_train)
-    print("training model 4.")
-    nn.fit(X_train, y_train)
-    
-    knn_disp = plot_roc_curve(knn, X_test, y_test)
-    svc_disp = plot_roc_curve(svc, X_test, y_test, ax=knn_disp.ax_)
-    nn_disp = plot_roc_curve(nn, X_test, y_test, ax=svc_disp.ax_)
-    rfc_disp = plot_roc_curve(model, X_test, y_test, ax=nn_disp.ax_)
-    rfc_disp.figure_.suptitle("Binding link - ROC curve comparison")
-    plt.show()
-if __name__ == "__main__":
-   main("../data/preprocessed_nnDataInput.csv")
-   
-   
-   
-   
-   
+from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import auc
+from sklearn.metrics import roc_curve
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+
+
+#prepare the logger
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Input file path")
+parser.add_argument("-o", "--outfolder", help="Output folder path")
+parser.add_argument("-p", "--profile", default="ipy_profile",
+                 help="Name of IPython profile to use")
+args = parser.parse_args()
+loc=os.path.join(args.outfolder,"results/")
+
+
+
+FILE_DIR =os.path.abspath(loc)
+if not os.path.exists(FILE_DIR):
+    os.mkdir( loc )
+sys.path.append(FILE_DIR)
+
+profile = args.profile
+inname = os.path.splitext(os.path.basename(args.input))[0]
+logout = inname+"_"+'RF_gs.log'
+logging.basicConfig(filename=os.path.join(FILE_DIR,logout),
+                    filemode='w',
+                    format='%(asctime)s - %(message)s',
+                    level=logging.DEBUG)
+logging.info("number of CPUs found: {0}".format(cpu_count()))
+logging.info("args.profile: {0}".format(profile))
+
+#prepare the engines
+c = Client(profile=profile)
+#The following command will make sure that each engine is running in
+# the right working directory to access the custom function(s).
+c[:].map(os.chdir, [FILE_DIR]*len(c))
+logging.info("c.ids :{0}".format(str(c.ids)))
+bview = c.load_balanced_view()
+register_parallel_backend('ipyparallel',
+                          lambda : IPythonParallelBackend(view=bview))
+
+#Get data
+inputT = pd.read_csv(args.input,sep=",",keep_default_na=False)
+
+
+#Parameters to test in parallel
+param_grid = [
+  {'n_estimators': [10, 10, 100, 1000,10000,100000, 1000000], 
+   'criterion': ['gini','entropy'],
+   'max_depth': [None, 2,4,5,8,10],
+   'max_features': ['sqrt', 'log2'],
+   'class_weight':['balanced',None]},
+ ]
+logging.info("Grid search parameters: {0}:".format(param_grid))
+# The scorers can be either be one of the predefined metric strings or a scorer
+# callable, like the one returned by make_scorer
+scoring = {'AUC': 'roc_auc', 'Accuracy': make_scorer(accuracy_score)}
+# Setting refit='AUC', refits an estimator on the whole dataset with the
+# parameter setting that has the best cross-validated AUC score.
+# That estimator is made available at ``gs.best_estimator_`` along with
+# parameters like ``gs.best_score_``, ``gs.best_params_`` and
+# ``gs.best_index_``
+gs = GridSearchCV(RandomForestClassifier(random_state=42),
+                  param_grid=param_grid,
+                  scoring=scoring, refit='AUC', 
+                  return_train_score=True,
+                  cv=5,
+                   n_jobs=len(c))
+X = inputT.loc[:,"ARACNE_score":"irp_score"]
+y = inputT["Link"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Run grid search
+with parallel_backend('ipyparallel'):
+    gs.fit(X_train, y_train)
+# extract results
+results = gs.cv_results_
+results = pd.DataFrame(results)
+resout= inname+"_"+'scores_opt_RF.csv'
+results.to_csv(os.path.join(FILE_DIR,resout))
+
+logging.info("Best estimator: {0}".format(gs.best_estimator_))
+logging.info("Avg. AUC score using Cross Validation (10): {0}".format(gs.best_score_))
+
+y_predict = gs.best_estimator_.predict(X_test)
+fpr, tpr, thresholds = roc_curve(y_test, y_predict)
+auc = auc(fpr, tpr)
+logging.info("AUC with the test data: ".format(auc))
+modelout = inname+"_"+'opt_RF.pickle'
+with open(os.path.join(FILE_DIR,modelout),'wb') as modelFile:
+    pickle.dump(gs.best_estimator_,modelFile)
+logging.info("process done.")
